d5c5261347b585a20e5fbcfe6ae04b8475fa5c64
Raynor <chodkos@gmail.com>
2017-06-19 13:57:06 +0200

    Hello Gicie xx

--- src/sages/bootcamp/git/GitHello.java
+++ src/sages/bootcamp/git/GitHello.java
@@ -4,4 +4,9 @@ package sages.bootcamp.git;
  * Created by Raynor on 2017-06-19.
  */
 public class GitHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
 }
